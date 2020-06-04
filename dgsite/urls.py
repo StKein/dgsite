800f1f72455b94188da16b9388bce18a4b22a251
vstakhanov <pfrazrabotkapf761@gmail.com>
2020-06-04 12:56:54 +0400

    reorganized site (huge commit, my bad again)

--- dgsite/urls.py
+++ dgsite/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls'))
+    path('blog/', include('blog.urls')),
+    path('polls/', include('polls.urls')),
+    path('', views.index, name='index'),
 ]
